468a7c6cb42f079df231d1eadfebc9a093e0783b
Anna Gąsiorowska <annanataliagasiorowska@gmail.com>
2023-09-20 12:42:30 +0200

    add method to print custom beans in console

--- src/main/java/com/wmcourse/warehouseapp/WarehouseAppApplication.java
+++ src/main/java/com/wmcourse/warehouseapp/WarehouseAppApplication.java
@@ -4,15 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import java.util.Arrays;
-
 @SpringBootApplication
 public class WarehouseAppApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(WarehouseAppApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(WarehouseAppApplication.class, args);
+		String[] beanNames = context.getBeanDefinitionNames();
+		for (String beanName : beanNames) {
+			Object bean = context.getBean(beanName);
 
+			// Check if the bean was created and configured by you
+			if (bean.getClass().getPackage().getName().startsWith("com.wmcourse.warehouseapp")) {
+				System.out.println("Bean Name: " + beanName);
+			}
+//		Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+		}
 
 	}
 }
--- src/main/java/com/wmcourse/warehouseapp/article/ArticleController.java
+++ src/main/java/com/wmcourse/warehouseapp/article/ArticleController.java
@@ -1,8 +1,6 @@
 package com.wmcourse.warehouseapp.article;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
 @Slf4j
-@Controller
 public class ArticleController {
 }
